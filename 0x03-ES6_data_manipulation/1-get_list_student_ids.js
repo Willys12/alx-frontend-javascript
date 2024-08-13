a4e3d9c7f69f4fd7b27d7acd3a5e61c30a806e55
Willys12 <ben.willys92@gmail.com>
2024-08-13 19:52:35 +0300

    More mapping

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,10 @@
+const getListStudentIds = (students) => {
+    if (!Array.isArray(students)) {
+        return [];
+    }
+
+    const studentIds = students.map(student => student.id);
+    return studentIds;
+};
+
+export default getListStudentIds;
